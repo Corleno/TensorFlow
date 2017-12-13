c5bf76a8d7ca972d86a835a7cba1edcea3e6dba0
Rui Meng <mengrui6351@gmail.com>
2017-12-12 21:09:56 -0800

    Add NN and word2vec algorithm

--- src/simple_eg.py
+++ src/simple_eg.py
@@ -20,8 +20,91 @@ a.get_shape()
 
 print (tf.reshape(a, (1,4)).eval())
 
+### Tensorflow variable
 W1 = tf.Variable(tf.zeros((2,2)), name = "weights")
 
 with tf.Session() as sess:
 	sess.run(tf.initialize_all_variables())
 	print (sess.run(W1))
+
+### add and multiple
+input1 = tf.constant(3.0)
+input2 = tf.constant(2.0)
+input3 = tf.constant(1.0)
+intermed = tf.add(input2, input3)
+mul = tf.multiply(input1, intermed)
+with tf.Session() as sess:
+	result = sess.run([mul, intermed])
+	print (result)
+
+### inputting Data
+a = np.zeros((3,3))
+ta = tf.convert_to_tensor(a)
+with tf.Session() as sess:
+	print(sess.run(ta))
+
+### Placeholders
+input1 = tf.placeholder(tf.float32)
+input2 = tf.placeholder(tf.float32)
+output = tf.multiply(input1, input2)
+with tf.Session() as sess:
+	print (sess.run([output], feed_dict={input1:[7.0], input2:[2.0]}))
+
+### Variable Scope
+with tf.variable_scope("foo"):
+	v = tf.get_variable("v", [1])
+with tf.variable_scope("foo", reuse = True):
+	v1 = tf.get_variable("v", [1])
+assert v1 == v
+
+### Linear Regression in TensorFlow
+import numpy as np
+import seaborn
+
+# Define input data
+X_data = np.arange(100, step=.1)
+y_data = X_data + 20*np.sin(X_data/10)
+
+# Plot input data
+# import matplotlib.pyplot as plt
+# plt.scatter(X_data, y_data)
+# plt.show()
+
+# Define data size and batch size
+n_samples = 1000
+batch_size = 100
+
+# Tensorflow is finicky about shapes, so resize
+X_data = np.reshape(X_data, (n_samples,1))
+y_data = np.reshape(y_data, (n_samples,1))
+
+# Define placeholders for input
+X = tf.placeholder(tf.float32, shape = (batch_size, 1))
+y = tf.placeholder(tf.float32, shape = (batch_size, 1))
+
+# Define variables to be learned
+with tf.variable_scope("linear-regression"):
+	W = tf.get_variable("weights", (1, 1),
+		initializer = tf.random_normal_initializer())
+	b = tf.get_variable("bias", (1,),
+		initializer = tf.constant_initializer(0.0))
+	y_pred = tf.matmul(X, W) + b
+	loss = tf.reduce_sum((y - y_pred)**2/n_samples)
+
+# Sample code to run one step of gradient desent
+opt = tf.train.AdamOptimizer()
+opt_operation = opt.minimize(loss)
+
+with tf.Session() as sess:
+	# Initializer Variables in graph
+	sess.run(tf.initialize_all_variables())
+	# Gradient descent loop for 500 steps
+	for _ in range(500):
+		# Select random minibatch
+		indices = np.random.choice(n_samples, batch_size)
+		X_batch, y_batch = X_data[indices], y_data[indices]
+		# Do gradient descent step
+		_, loss_val = sess.run([opt_operation, loss], feed_dict={X: X_batch, y: y_batch})
+		print (loss_val)
+
+#
---
+++ src/test.py
@@ -0,0 +1,4 @@
+import numpy as np 
+a = np.ndarray(shape=(10), dtype=np.int16)
+b = np.ndarray(shape=(10, 1), dtype=np.int16)
+print(np.random.choice(100,10,replace=False))
